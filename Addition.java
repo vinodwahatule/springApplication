7feae4edca576832fdf9d5c8522d2488b66c5db0
vinodwahatule <vinodwahatule9325@gmail.com>
2023-12-16 11:42:33 +0530

    new file created

--- Addition.java
+++ Addition.java
@@ -2,6 +2,10 @@ public class Addition
 {
 
 public static void main(String[] args){
+int a=20;
+int b=30;
+int c=a+b;
+System.out.println(c);
 
 }
 
---
+++ substraction.java
@@ -0,0 +1,14 @@
+public substraction{
+
+public static  void main(Strings[] args)
+{
+
+int a=30;
+int b=40;
+int c=a-b;
+
+}
+
+
+
+}
